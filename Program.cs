714d2029fd01c914225d5101f8b0079e1901d259
Yaimangmum <jirayu.majesty@mail.kmutt.ac.th>
2022-09-09 11:40:07 +0700

    raiwa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        User data = new User();
+        Console("Name : ");
+        data.Name = Console.ReadLine();
+        Console.Write("Surname : ");
+        data.Surname = Console.ReadLine();
+        Consoel.Write("ID : ");
+        data.ID = StringToLong(Console.ReadLine());
+        Console.Write("Score : ");
+        data.Score = StringToInt(Console.ReadLine());
+    }
+    public static int StringToInt(string value)
+    {
+        if(int.TryParse(value,out int number))
+        {
+            return number;
+        }
+        throw new Exception("Wrong Word");
+    }
+        public static long StringToLong(string value)
+    {
+        if(long.TryParse(value,out long number))
+        {
+            return number;
+        }
+        throw new Exception("Wrong Word");
+    }
+}
---
+++ User.cs
@@ -0,0 +1,12 @@
+﻿class User
+{
+    public string Name = "";
+    public string Surname = "";
+    public long ID;
+    public int Score;
+    public static void PrintInputInformation(User data)
+    {
+        Console.Write("{0} {1} {2} {3} {4}",data.Name,data.Surname,data.ID,data.Score);
+    }
+
+}
